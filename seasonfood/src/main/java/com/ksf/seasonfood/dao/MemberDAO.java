1af8f4b5dc4ef40f5b3911bfcf6a2ed7bd5e3fe5
SunnySunn123 <songsunhwa@gmail.com>
2021-06-15 10:09:35 +0900

    Revert "로그인 작업"
    This reverts commit 02063918a5bb73bca10a6bd211975d896b7b3095, reversing
    changes made to 82873c7260e12ebca92eb45fa7a202676aab77b8.

--- seasonfood/src/main/java/com/ksf/seasonfood/dao/MemberDAO.java
+++ seasonfood/src/main/java/com/ksf/seasonfood/dao/MemberDAO.java
@@ -1,10 +1,15 @@
 package com.ksf.seasonfood.dao;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
+import javax.servlet.http.HttpSession;
 
-@Mapper
-@Repository
-public interface MemberDao {
+import com.ksf.seasonfood.dto.MemberVO;
 
+public interface MemberDAO {
+
+	// 회원 로그인 체크
+	public boolean loginCheck(MemberVO vo);
+	// 회원 로그인 정보
+	public MemberVO viewMember(MemberVO vo) ;
+		
+	public void logout(HttpSession session);
 }
--- seasonfood/src/main/resources/static/js/index.js
+++
@@ -1,8 +0,0 @@
-function showProfile() {
-  let dropDown = document.getElementById("dropDownMenu");
-  if (dropDown.style.display === "none") {
-    dropDown.style.display = "block";
-  } else {
-    dropDown.style.display = "none";
-  }
-}
